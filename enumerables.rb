c17f6648b32865547a4aea623313317eb59b9d4b
Tendongze Godson <tendongze95@gmail.com>
2020-06-03 11:29:39 +0100

    built the my_each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -6,4 +6,14 @@ module Enumerable
         i += 1
     end
   end
+  def my_each_with_index
+    i = 0
+    while i < self.size
+        yield(self[i], i)
+        i += 1
+    end
+    self
+  end
+
+ 
 end
