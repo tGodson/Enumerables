da31a05dc3f5080bd3531b28db730d03f7ad82cd
Tendongze Godson <tendongze95@gmail.com>
2020-06-04 17:42:50 +0100

    fixed the lint errors and deleted the node module

--- enumerables.rb
+++ enumerables.rb
@@ -1,17 +1,17 @@
 module Enumerable
   def my_each
     i = 0
-    while i < self.size
-        yield(self[i])
-        i += 1
+    while i < size
+      yield(self[i])
+      i += 1
     end
   end
 
   def my_each_with_index
     i = 0
-    while i < self.size
-        yield(self[i], i)
-        i += 1
+    while i < size
+      yield(self[i], i)
+      i += 1
     end
     self
   end
@@ -19,40 +19,32 @@ module Enumerable
   def my_select
     invited_list = []
 
-    self.my_each do |x|
-      if yield(x)
-        invited_list.push(x)
-      end
+    my_each do |x|
+      invited_list.push(x) if yield(x)
     end
 
     invited_list
   end
 
   def my_all?
-    self.my_each do |x|
-      if !yield(x)
-        return false
-      end
+    my_each do |x|
+      return false unless yield(x)
     end
 
     true
   end
 
   def my_any?
-    self.my_each do |el|
-      if yield(el)
-        return true
-      end
+    my_each do |el|
+      return true if yield(el)
     end
 
     false
   end
 
   def my_none?
-    self.my_each do |el|
-      if yield(el)
-        return false
-      end
+    my_each do |el|
+      return false if yield(el)
     end
 
     true
@@ -60,45 +52,33 @@ module Enumerable
 
   def my_count
     count = 0
-    self.my_each { |i|
-        if yield(i) == true
-            count += 1
-        end
-    }
+    my_each do |i|
+      count += 1 if yield(i) == true
+    end
     count
   end
-  
-  def my_map(proc = nil, &block)
-    new_array = []
-    counter = 0
-    while counter < self.length
-      if proc && block_given?
-        new_array << block.call(proc.call(self.to_a[counter]))
-      elsif block_given?
-        new_array << block.call(self.to_a[counter])
-      elsif proc
-        new_array << proc.call(self.to_a[counter])
-      else
-        return self.to_enum(:my_map) if !block_given?
-      end
-      counter += 1
+
+  def my_map
+    mapped = []
+    my_each do |i|
+      mapped << (proc.nil? ? proc.call(i) : yield(i))
     end
-    new_array
+    mapped
   end
 
   def my_inject(arg = nil, &block)
     memo = arg if arg
-    self.my_each do |element|
-      if memo
-        memo = block.call(memo, element)
-      else
-        memo = element
-      end
+    my_each do |element|
+      memo = if memo
+               block.call(memo, element)
+             else
+               element
+             end
     end
     memo
   end
-  
+
   def multiply_els(arr)
-      arr.my_inject {|x,y| x*y}
+    arr.my_inject { |x, y| x * y }
   end
 end
